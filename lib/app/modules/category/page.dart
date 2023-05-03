645b0f1f9cacb4b0919a3ab45f2fc7b19009f342
Fernando Gnadt de Oliveira <fernando.oliveira@mobilus.com.br>
2023-05-02 23:35:53 -0300

    desafio novo produto celular

--- lib/app/modules/category/page.dart
+++ lib/app/modules/category/page.dart
@@ -1,3 +1,4 @@
+import 'package:app_painel_hortifruti_pratico/app/routes/routes.dart';
 import 'package:app_painel_hortifruti_pratico/app/widgets/category/category_controller.dart';
 import 'package:app_painel_hortifruti_pratico/app/widgets/category/category_widget.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +10,20 @@ class CategoryPage extends GetView<CategoryController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Produtos da Categoria')),
+      appBar: AppBar(
+        title: const Text('Produtos da Categoria'),
+        actions: [
+          TextButton(
+            onPressed: () => Get.toNamed(
+              Routes.product,
+              parameters: {
+                'category_id': controller.categoryId.value.toString()
+              },
+            ),
+            child: const Text('Novo Produto'),
+          ),
+        ],
+      ),
       body: CategoryWidget(),
     );
   }
